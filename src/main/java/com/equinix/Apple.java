1dcfd856a1fff3900593ad9d39eb59aec4debdcd
Balaji Kesavan <opencloud.balaji@gmail.com>
2017-03-21 22:42:24 +0800

    Initial Commit

---
+++ src/main/java/com/equinix/Apple.java
@@ -0,0 +1,18 @@
+package com.equinix;
+
+import java.util.*;
+
+/**
+ * Created by bkesavan on 20-Mar-17.
+ */
+public class Apple {
+  String color=new String();
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/main/java/com/equinix/Java8PocApplication.java
@@ -0,0 +1,53 @@
+package com.equinix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class Java8PocApplication {
+
+    public static boolean isGreenApple(Apple apple) {
+        return "GREEN".equals(apple.getColor());
+    }
+
+    public interface Inter<T> {
+        boolean test(T t);
+    }
+
+    static List<Apple> filterApples(List<Apple> inventory, Inter<Apple> app) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (app.test(apple)) {
+                result.add(apple);
+            }
+        }
+        System.out.println("result = " + result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8PocApplication.class, args);
+
+        Apple a1 = new Apple();
+        Apple a2 = new Apple();
+        Apple a3 = new Apple();
+        Apple a4 = new Apple();
+
+        a1.setColor("RED");
+        a2.setColor("GREEN");
+        a3.setColor("ORANGE");
+        a4.setColor("GREEN");
+
+        List<Apple> app = new ArrayList();
+        app.add(a1);
+        app.add(a2);
+        app.add(a3);
+        app.add(a4);
+
+        //filterApples(app, Apple::isGreenApple);
+        filterApples(app, (Apple a) -> "GREEN".equals(a.getColor()) );
+    }
+}
